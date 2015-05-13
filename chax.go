b0cbdff7f9403c1ec746fb7e0fbbc0007f7b3f44
Eric Myhre <hash@exultant.us>
2015-05-13 16:53:13 -0700

    Make a window.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- chax.go
+++ chax.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/themes/dark"
+)
+
 func main() {
+	gl.StartDriver(appMain)
+}
+
+func appMain(driver gxui.Driver) {
+	theme := dark.CreateTheme(driver)
+
+	window := theme.CreateWindow(800, 600, "Hallo mundai")
+	window.OnClose(driver.Terminate)
 }
