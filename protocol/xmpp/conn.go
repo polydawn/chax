032f623fb969bb6086846c0a22812c365e256e0b
Eric Myhre <hash@exultant.us>
2015-05-19 17:42:01 -0700

    Beginning to parse more of the things.
    Signed-off-by: Eric Myhre <hash@exultant.us>

--- protocol/xmpp/conn.go
+++ protocol/xmpp/conn.go
@@ -1,6 +1,7 @@
 package xmpp
 
 import (
+	"fmt"
 	"time"
 
 	"polydawn.net/chax/protocol"
@@ -37,15 +38,47 @@ func (conn *Conn) run() {
 		}
 	}()
 	// expect to do heartbeats and maintenance periodically
-	heartbeatTicker := time.NewTicker(5 * time.Second)
+	heartbeatTicker := time.NewTicker(10 * time.Second)
 	// main loop
 	for {
 		select {
 		case <-heartbeatTicker.C:
 			conn.raw.SignalPresence("")
 		case rawStanza := <-stanzaChan:
-			// TODO ... many branches of processing
-			conn.log.Debug("unhandled stanza", "stanza", rawStanza)
+			switch stanza := rawStanza.Value.(type) {
+			case *xmpp.ClientMessage:
+				conn.log.Debug("unhandled message stanza", "stanza", stanza)
+			case *xmpp.ClientPresence:
+				conn.log.Debug("unhandled presence stanza", "stanza", stanza)
+			case *xmpp.ClientIQ:
+				conn.log.Debug("unhandled IQ request stanza", "stanza", stanza)
+				if stanza.Type != "get" && stanza.Type != "set" {
+					continue
+				}
+				// TODO control the spiraling inanity of this parsing
+				// reply := s.processIQ(stanza)
+				var reply interface{}
+				// TODO also this does not seem to me like it should necessarily be done while blocking the main event loop; review
+				if reply == nil {
+					reply = xmpp.ErrorReply{
+						Type:  "cancel",
+						Error: xmpp.ErrorBadRequest{},
+					}
+				}
+				if err := conn.raw.SendIQReply(stanza.From, "result", stanza.Id, reply); err != nil {
+					conn.log.Warn("failed to send IQ message", "reply", reply, "error", err)
+				}
+			case *xmpp.StreamError:
+				var text string
+				if len(stanza.Text) > 0 {
+					text = stanza.Text
+				} else {
+					text = fmt.Sprintf("%s", stanza.Any)
+				}
+				conn.log.Warn("error from server", "stanza", text)
+			default:
+				conn.log.Warn("unrecognized stanza", "name", rawStanza.Name, "value", rawStanza.Value)
+			}
 		case cmd := <-conn.commandChan:
 			switch cmd := cmd.(type) {
 			case func():
